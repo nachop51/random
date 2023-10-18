e58cdbf863317094baf1a53cc87556079be250d9
Nachop51 <nachoperalta0@gmail.com>
2023-10-17 21:22:37 -0300

    whoops

---
+++ calculator/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+
+    std::string str;
+
+    std::ifstream file("file.txt");
+
+    return 0;
+}
